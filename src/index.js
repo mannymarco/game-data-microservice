a99280893834c1f20df6c973faf0f66f545a276f
mattdotmac <matt.ashdene@icloud.com>
2019-08-05 23:28:42 +0100

    Set port to 8080

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('./db/mongoose');
+const gameRouter = require('./routes/game');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(gameRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
